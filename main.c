7001f4956a1a72007d433eacc92f352016e3a2b4
Yannvdm <129206015+Yannvdm@users.noreply.github.com>
2025-01-09 18:05:43 +0100

    Update main.c

--- main.c
+++ main.c
@@ -1,6 +1,6 @@
-#include "gp_championship.h"
+#include "championship.h"
 
-int main(int argc, char **argv) {
-    startGrandPrix();
+int main(int argc, char **argv){
+    championship();
     exit(0);
 }
